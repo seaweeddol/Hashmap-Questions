b28b01d4bb4ea7a707172c01ae87f16c20f6033e
Jessica Stone <jstone4815@gmail.com>
2020-03-29 12:55:58 -0700

    pass tests with array

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,14 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  new_array = []
+
+  list1.each do |num|
+    list2.each do |num2|
+      if num == num2
+        new_array << num
+        break
+      end
+    end
+  end
+
+  return new_array
 end
