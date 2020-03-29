1a02ee3f16a7cee77d8fb6bb64b4b66c03b470c1
Jessica Stone <jstone4815@gmail.com>
2020-03-29 13:09:43 -0700

    pass all tests for permutations

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,22 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  return false if string1.length != string2.length
+
+  hash = {}
+  string1.length.times do |i|
+    if hash[string1[i]]
+      hash[string1[i]] += 1
+    else
+      hash[string1[i]] = 1
+    end
+  end
+
+  string2.length.times do |i|
+    if hash[string2[i]] && hash[string2[i]] > 0
+      hash[string2[i]] -= 1
+    else
+      return false
+    end
+  end
+
+  return true
 end
