79a4e4090c90bc6d718e7d1341dcda829557deea
Jessica Stone <jstone4815@gmail.com>
2020-03-29 13:26:05 -0700

    pass all tests for palindrome_permutation

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,23 @@
-
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  hash = {}
+
+  string.length.times do |i|
+    if hash[string[i]]
+      hash[string[i]] += 1
+    else
+      hash[string[i]] = 1
+    end
+  end
+
+
+  odd = 0
+  hash.each do |k, v|
+    if v % 2 != 0
+      if v == 1
+        return false if (odd += 1) > 1
+      end
+    end
+  end
+
+  return true
 end
